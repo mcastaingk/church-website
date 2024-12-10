f78cde129fd2cf8abba965917891e770f68ba6e3
Miranda Castaing <mcastaingk@ucenfotec.ac.cr>
2024-12-09 22:35:33 -0600

    Upload video1.mp4 to YouTube

--- script-testimonios.js
+++ script-testimonios.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const carousel = document.querySelector('.testimonies-carousel');
     const carouselItems = document.querySelectorAll('.testimonies-carousel-item');
     const videoPlayer = document.getElementById('video-player');
+    const youtubeVideoPlayer = document.getElementById('youtube-video-player');
     const blurredBackground = document.querySelector('.blurred-background');
 
     let currentIndex = 0;
@@ -30,7 +31,15 @@ document.addEventListener('DOMContentLoaded', () => {
             item.classList.add('selected');
 
             const videoSrc = item.getAttribute('data-video');
-            videoPlayer.querySelector('source').src = `assets/${videoSrc}`;
+            if (videoSrc.includes('.mp4')) {
+                videoPlayer.querySelector('source').src = `assets/${videoSrc}`;
+                videoPlayer.style.removeProperty('display');
+                youtubeVideoPlayer.style.display = `none`;
+            } else {
+                youtubeVideoPlayer.src = `${videoSrc}`;
+                youtubeVideoPlayer.style.removeProperty('display');
+                videoPlayer.style.display = `none`;
+            }
             videoPlayer.load();
 
             const thumbnail = item.src;
@@ -41,4 +50,5 @@ document.addEventListener('DOMContentLoaded', () => {
     // Set initial video
     carouselItems[0].classList.add('selected');
     blurredBackground.style.backgroundImage = `url(${carouselItems[0].src})`;
+    videoPlayer.style.display = `none`;
 });
